42ec58f215bf1e15f1f525a13b14df4439c6221b
ctrlKshav <keshavm9978@gmail.com>
2024-09-27 00:49:29 +0530

    huh

--- frontend/src/components/EventComp.jsx
+++ frontend/src/components/EventComp.jsx
@@ -5,8 +5,8 @@ import 'dotenv/config'
 
 const EventComp = () => {
   
-  const CLIENT_ID = process.env.CLIENT_ID
-  const API_KEY = process.env.API_KEY
+  const CLIENT_ID = import.meta.env.CLIENT_ID
+  const API_KEY = import.meta.env.API_KEY
   const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
   const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
